4ec07f1f092477d477c9a5f3f0891be627188416
selwyntheo <selwyn.theo@gmail.com>
2015-11-20 04:30:07 -0500

    First Commit

---
+++ src/app/app.js
@@ -0,0 +1,29 @@
+(function () {
+    'use strict';
+    var app = angular.module('eliteAdmin', [
+        // Angular modules
+        'ngRoute',
+        'psSecurityMonitorService'
+
+        // 3rd Party Modules
+        
+    ]);
+
+    app.config(['$routeProvider', configRoutes]);
+
+    function configRoutes($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'app/home/home.html',
+                controller: 'HomeCtrl',
+                controllerAs: 'vm'
+            });
+
+
+        $routeProvider.otherwise('/');
+    }
+
+    app.run(['$route', function ($route) {
+        // Include $route to kick start the router.
+    }]);
+})();
